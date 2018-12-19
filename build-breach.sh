87c58915326ae9f079ef42ca2365b70e94eed496
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2018-12-19 06:56:55 -0700

    Build-Breach-Comp
    Downloads the breach compilation database

---
+++ build-breach.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# BreachCompilation download
+# Run this script to install transmission and download
+# the database into a subfolder called BreachCompilation.
+3 Magnet file is: magnet:?xt=urn:btih:7ffbcd8cee06aba2ce6561688cf68ce2addca0a3&dn=BreachCompilation&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fglotorrents.pw%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337
+
+
+command -v transmission || apt update; apt install transmission transmission-daemon -y
+transmission-daemon --download-dir ./
+transmission-daemon
+sleep 5
+transmission-remote -a magnet:?xt=urn:btih:7ffbcd8cee06aba2ce6561688cf68ce2addca0a3&dn=BreachCompilation&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fglotorrents.pw%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337
+sleep 5
+transmission-remote -l
+
